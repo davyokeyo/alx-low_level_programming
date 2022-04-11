fdbc99d6682ef91e9c85eba411bc52571fc0a129
davyokeyo <daveokeyo97@gmail.com>
2022-04-10 21:50:07 -0700

    file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - prints the name of the file and new line.
+ *
+ * Return: Always zero = success.
+ *
+ */
+int main(void)
+{
+printf("%s\n", _FILE_);
+return (0);
+}
