3db2158195f14fd7e3dcb913c2785612e9eb09cd
davyokeyo <daveokeyo97@gmail.com>
2022-04-21 12:54:10 -0700

    Task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,18 +1,16 @@
 /*
- * File: 100-first.c
+ *File: 100-first.c
  * 
  */
-
 #include <stdio.h>
 
 void __attribute__((constructor)) hare(void);
 
 /**
- * hare - Prints a string before the
- *        main function is executed.
+ *hare - Prints a string before the main function is executed.
+ *
  */
 void hare(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-	       "I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n""I bore my house upon my back!\n");
 }
